f52277d92c0ce28455f5355ec49b4a4fcba7796d
axeltb <axeltb@stud.ntnu.no>
2020-04-23 18:33:31 +0200

    Added example histogram

--- Iris/histogram.py
+++ Iris/histogram.py
@@ -1,5 +1,10 @@
 # start
 
 import numpy as np
+import matplotlib.pyplot as plt
 
+testArray = [1, 4, 9, 26, 25, 36, 49, 64, 81, 100, 10, 20, 60, 70, 80, 90, 100]
+bins = [0, 33, 66, 100]
 
+plt.hist(testArray, bins)
+plt.show()
