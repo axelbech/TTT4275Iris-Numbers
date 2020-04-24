7696006f4911cb2e0c594b7189b6af4e0fa8a6f1
axeltb <axeltb@stud.ntnu.no>
2020-04-24 17:17:01 +0200

    Small changes to MSEtrainer

--- Iris/MSEtrainer.py
+++ Iris/MSEtrainer.py
@@ -5,3 +5,9 @@ def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
 maxiter = 300
+
+WFromTraining(maxiter = 100, alpha = 0.1, nclass, trainingData, solution): # training data must be on the format ???
+    # nsamples = ??
+    # nfeatures = ??
+    # Initialize W (randomly, ones, identity?)
+    for it in range(300): #perharps also another termination criteria?
