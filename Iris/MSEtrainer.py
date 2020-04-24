f4cf59ae9c3083741076b3c2850d04b695d04cd8
axeltb <axeltb@stud.ntnu.no>
2020-04-24 19:12:19 +0200

    Started defining stuctures in MSEtrainer

--- Iris/MSEtrainer.py
+++ Iris/MSEtrainer.py
@@ -4,10 +4,18 @@ import math
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-maxiter = 300
+def WFromTraining(nclasses, trainingData, solution, maxiter = 100, alpha = 0.1): # training data must be on the format ???
+    dataDims = np.shape(trainingData)
+    nsamples = dataDims[0]
+    nfeatures = dataDims[1]
 
-WFromTraining(maxiter = 100, alpha = 0.1, nclass, trainingData, solution): # training data must be on the format ???
-    # nsamples = ??
-    # nfeatures = ??
-    # Initialize W (randomly, ones, identity?)
-    for it in range(300): #perharps also another termination criteria?
+    paddedData = (np.append((trainingData.T),np.ones(1,nsamples))).T # add ones to the end of the feature samples for the modified form
+
+    W = np.ones((nclasses, nfeatures + 1))# Initialize W (randomly, ones, identity?). Not the added column for the modified form
+    g = np.empty(nsamples,nclasses)
+    for totalIt in range(maxiter): #perharps also another termination criteria?
+        for sampleIt in range(nsamples):
+            xCurrent = W*((paddedData[sampleIt,:]).T) # z_i = W * x_i
+            for classIt in range(nclasses):
+                g[sampleIt, classIt] = sigmoid(xCurrent[classIt]) # g_i = sigmoid(z_i)
+    print('heyoo')
