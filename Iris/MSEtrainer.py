0a64165afb8fff99d5a4a9e87d7cd95aa46930ee
axeltb <axeltb@stud.ntnu.no>
2020-04-26 16:35:51 +0200

    Now have a working MSEtrainer, prolly finished with MSE calculator

--- Iris/MSEtrainer.py
+++ Iris/MSEtrainer.py
@@ -4,7 +4,7 @@ import math
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-def WFromTraining(trainingData, solution, maxiter = 100, alpha = 0.1): # training data must be on the format ???
+def WFromTraining(trainingData, solution, maxiter = 300, alpha = 0.1): # training data must be on the format ???
     dataDims = np.shape(trainingData)
     nsamples = dataDims[0]
     nfeatures = dataDims[1]
@@ -13,21 +13,20 @@ def WFromTraining(trainingData, solution, maxiter = 100, alpha = 0.1): # trainin
     nclasses = solDims[1]
 
     paddedData = (np.concatenate(((trainingData.T),np.ones((1,nsamples))),axis=0)).T # add ones to the end of the feature samples for the modified form
-    print(paddedData)
 
-    W = np.ones((nclasses, nfeatures + 1)) # Initialize W (randomly, ones, identity?). Not the added column for the modified form
-    g = np.empty((nsamples,nclasses))
+    W = 0.5 * np.random.rand(nclasses, nfeatures + 1) - 0.25 # Initialize W randomly OBS choice of start W can be crucial
+    g = np.zeros((nsamples,nclasses))
     for totalIt in range(maxiter): # perharps also another termination criteria?
         nabW = np.zeros((nclasses, nfeatures + 1))
-
         for sampleIt in range(nsamples):
-            xCurrent = np.dot(W,(paddedData[sampleIt,:]).T) # z_i = W * x_i
+            xCurrent = (np.array(paddedData[sampleIt,:],ndmin=2)).T
+            zCurrent = np.dot(W,xCurrent) # z_i = W * x_i
             for classIt in range(nclasses):
-                g[sampleIt, classIt] = sigmoid(xCurrent[classIt]) # g_i = sigmoid(z_i)
+                g[sampleIt, classIt] = sigmoid(zCurrent[classIt,:]) # g_i = sigmoid(z_i)
 
-            g_k = (g[sampleIt,:]).T
-            t_k = (solution[sampleIt,:]).T
-            x_k = (paddedData[sampleIt,:]).T
+            g_k = (np.array((g[sampleIt,:]), ndmin=2)).T
+            t_k = (np.array((solution[sampleIt,:]), ndmin=2)).T
+            x_k = xCurrent
 
             # Sum element [(g_k-t_k)◦g_k◦(1-g_k)]x_k.T
             nabW += np.dot(np.multiply(np.multiply(g_k-t_k,g_k),np.ones((nclasses,1))-g_k),x_k.T)
--- Iris/tester.py
+++ Iris/tester.py
@@ -1,9 +1,46 @@
+import numpy as np
+
 from importdata import importdata
 from MSEtrainer import WFromTraining
+from MSEtrainer import sigmoid
 
 samplesAndSolutions = importdata(nsamples = 150, nclasses = 3, nfeatures = 4, filePath = 'Data/iris.data')
 
 samples = samplesAndSolutions[0]
 solutions = samplesAndSolutions[1]
 
-W = WFromTraining(samples, solutions)
+W = WFromTraining(samples, solutions, alpha=0.01, maxiter=1000)
+
+
+def MSE(testSamples, testSolutions, W):
+    dataDims = np.shape(testSamples)
+    nsamples = dataDims[0]
+
+    solDims = np.shape(testSolutions)
+    nclasses = solDims[1]
+
+    paddedData = (np.concatenate(((testSamples.T),np.ones((1,nsamples))),axis=0)).T
+    g = np.zeros((nsamples,nclasses))
+
+    MSE = 0
+
+    for sampleIt in range(nsamples):
+        xCurrent = (np.array(paddedData[sampleIt,:],ndmin=2)).T
+        zCurrent = np.dot(W,xCurrent) # z_i = W * x_i
+        for classIt in range(nclasses):
+            g[sampleIt, classIt] = sigmoid(zCurrent[classIt,:]) # g_i = sigmoid(z_i)
+
+        g_k = (np.array((g[sampleIt,:]), ndmin=2)).T
+        t_k = (np.array((testSolutions[sampleIt,:]), ndmin=2)).T
+        x_k = (np.array((paddedData[sampleIt,:]), ndmin=2)).T
+
+        MSE += 0.5 * (np.dot(((g_k-t_k).T),g_k-t_k))
+
+    return MSE
+
+ourMSE = MSE(samples,solutions,W)
+
+randomMSE = MSE(samples,solutions, 1 * np.random.rand(3, 5) - 0.5)
+
+print('MSE from our W = ', ourMSE)
+print('MSE from random W = ', randomMSE)
