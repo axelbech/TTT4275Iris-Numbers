bbf94207cf789e35cc18f44561fe33830db20137
axelbech <axeltb@stud.ntnu.no>
2020-04-30 15:36:15 +0200

    Added function to print confusion matrices in Latex-friendly format

---
+++ Numbers/printConfusion.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def print_confusion(conf):
+    """
+    Creates a latex table that is pre-formatted for a given confusion matrix.
+    """
+    classes = ['1', '2', '3']
+
+
+    print("""\\begin{table}[H]
+\\caption{}
+\\centering
+\\begin{tabular}{|c|lll|}""")
+    conf = conf.astype(int)
+    print('\\hline\nclass & '+' & '.join(classes) + '\\\\' + '\\hline')
+    for i, row in enumerate(conf):
+        rw = classes[i]
+        for j, elem in enumerate(row):
+            rw += ' & '
+            if elem == 0:
+                rw += '-'
+            else:
+                rw += str(elem)
+        rw += '\\\\'
+        if i == 2:
+            rw += '\\hline'
+        print(rw)
+    print("""\\end{tabular}
+\\end{table}""")
+    print()
